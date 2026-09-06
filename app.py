932a823dd76adea33d46c14128b9a3512efa29aa
Sherline Bertrand <sherlinebertrand@gmail.com>
2026-09-05 23:28:48 -0400

    started process to connect flask to a database

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify, request
-
+import sqlite3
 app = Flask(__name__)
+
+def get_db_connection():
+    conn= sqlite3.connect('products.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 products = [
         {"id":1, "name": "Keyboard", "price":49.99},
