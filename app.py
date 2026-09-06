ac1c0e89c445386a175a3199e89dfff0226d5d3b
Sherline Bertrand <sherlinebertrand@gmail.com>
2026-09-05 23:24:53 -0400

    rest API

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+products = [
+        {"id":1, "name": "Keyboard", "price":49.99},
+        {"id":2, "name": "Mouse", "price":29.99}
+    ]
 
 @app.route("/")
 def home():
@@ -8,12 +13,17 @@ def home():
 
 @app.route("/products", methods=["GET"])
 def get_products():
-    products = [
-        {"id":1, "name": "Keyboard", "price":49.99},
-        {"id":2, "name": "Mouse", "price":29.99}
-    ]
     return jsonify(products)
 
-
+@app.route("/products", methods =["POST"])
+def add_products():
+    data = request.get_json()
+    new_product = {
+        "id": len(products) + 1,
+        "name": data.get("name"),
+        "price":data.get("price")
+    }
+    products.append(new_product)
+    return jsonify({"message": "Product added", "product": new_product}), 201
 if __name__ =="__main__":
     app.run(debug=True)
