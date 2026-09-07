2d4a172dda3e991b6baa9eca20faaa7c02296ee6
Sherline Bertrand <sherlinebertrand@gmail.com>
2026-09-06 20:33:04 -0400

    connected flask to a database

--- app.py
+++ app.py
@@ -3,14 +3,26 @@ import sqlite3
 app = Flask(__name__)
 
 def get_db_connection():
-    conn= sqlite3.connect('products.db')
+    import os
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "products.db")
+    conn= sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
 
-products = [
-        {"id":1, "name": "Keyboard", "price":49.99},
-        {"id":2, "name": "Mouse", "price":29.99}
-    ]
+@app.route("/init", methods=["GET"])
+def init_db():
+    conn = get_db_connection()
+    conn.execute("""
+                CREATE TABLE IF NOT EXISTS products (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                price REAL NOT NULL
+                )
+                 """)
+    conn.commit()
+    conn.close()
+    return jsonify({"message": "Database Init complete"})
 
 @app.route("/")
 def home():
@@ -18,17 +30,31 @@ def home():
 
 @app.route("/products", methods=["GET"])
 def get_products():
-    return jsonify(products)
+    conn = get_db_connection()
+    rows = conn.execute("SELECT * FROM products").fetchall()
+    conn.close()
+    return jsonify([dict(row) for row in rows])
 
 @app.route("/products", methods =["POST"])
 def add_products():
     data = request.get_json()
+    name = data.get("name")
+    price = data.get("price")
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO products (name, price) Values(?,?)", (name,price))
+    conn.commit()
+    new_id = cursor.lastrowid
+    conn.close()
+
     new_product = {
-        "id": len(products) + 1,
-        "name": data.get("name"),
-        "price":data.get("price")
+        "id": new_id,
+        "name": name,
+        "price":price
     }
-    products.append(new_product)
     return jsonify({"message": "Product added", "product": new_product}), 201
+
 if __name__ =="__main__":
+    with app.app_context():
+        init_db()
     app.run(debug=True)
