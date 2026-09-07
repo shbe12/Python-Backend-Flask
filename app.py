0ff86df5a7d181428ecab901d9e9941a8fa3917b
Sherline Bertrand <sherlinebertrand@gmail.com>
2026-09-06 23:46:51 -0400

    API AND FUNCTOOLS

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask, jsonify, request
 import sqlite3
 import hashlib
+import os
+import dotenv
+from functools import wraps
+
+API_TOKEN = os.getenv("API_TOKEN")
 
 app = Flask(__name__)
+
+def require_token(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        token = request.headers.get("Authorization")
+        if token != f"Bearer {API_TOKEN}":
+            return jsonify({"error": "Unauthorized"}), 401
+        return f(*args, **kwargs)
+    return decorated_function
 
 def get_db_connection():
     import os
@@ -45,6 +59,7 @@ def get_products():
     return jsonify([dict(row) for row in rows])
 
 @app.route("/products", methods =["POST"])
+@require_token
 def add_products():
     data = request.get_json()
     name = data.get("name")
